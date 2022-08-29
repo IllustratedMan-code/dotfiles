ad3b57838b1c214eac7df6fe8c68e3d443dc2051
lew2mz <david.lewis@cchmc.org>
2022-08-29 10:37:29 -0400

    added lualine config

---
+++ nvim/lua/lualine/themes/base16.lua
@@ -0,0 +1,57 @@
+local colors = {
+                 base00="2E3440",
+                 base01="3B4252",
+                 base02="434C5E",
+                 base03="4C566A",
+                 base04="D8DEE9",
+                 base05="E5E9F0",
+                 base06="ECEFF4",
+                 base07="8FBCBB",
+                 base08="BF616A",
+                 base09="D08770",
+                 base0A="EBCB8B",
+                 base0B="A3BE8C",
+                 base0C="88C0D0",
+                 base0D="81A1C1",
+                 base0E="B48EAD",
+                 base0F="5E81AC",
+                }
+local base16 = {}
+
+base16.normal = {
+	a = { fg = colors.base01, bg = colors.base0D },
+	b = { fg = colors.base05, bg = colors.base02 },
+	c = { fg = colors.base04, bg = colors.base01 },
+}
+
+base16.insert = {
+	a = { fg = colors.base01, bg = colors.base04 },
+	b = { fg = colors.base06, bg = colors.base02 },
+	y = {  fg = colors.base05, bg = colors.base02 },
+}
+
+base16.visual = {
+	a = { fg = colors.base00, bg = colors.base07 },
+	b = { fg = colors.base04, bg = colors.base02 },
+	y = {  fg = colors.base05, bg = colors.base02 },
+}
+
+base16.replace = {
+	a = { fg = colors.base00, bg = colors.base09 },
+	b = { fg = colors.base04, bg = colors.base02 },
+	y = {  fg = colors.base05, bg = colors.base02 },
+}
+
+base16.command = {
+	a = { fg = colors.base00, bg = colors.base0E, gui = "bold" },
+	b = { fg = colors.base04, bg = colors.base02 },
+	y = {  fg = colors.base05, bg = colors.base02 },
+}
+
+base16.inactive = {
+	a = { fg = colors.base04, bg = colors.base00, gui = "bold" },
+	b = { fg = colors.base04, bg = colors.base00 },
+	c = { fg = colors.base04, bg = colors.base01 },
+}
+
+return base16
