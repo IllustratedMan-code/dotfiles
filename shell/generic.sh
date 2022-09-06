4ad8dfffcca2abedeca798cf9b6e55436090bf25
lew2mz <david.lewis@cchmc.org>
2022-09-06 10:09:49 -0400

    automated git for this repo

--- shell/generic.sh
+++ shell/generic.sh
@@ -38,6 +38,7 @@ pdfinterleavebend ()
 }
 nrs ()
 {
+    (cd ~/dotfiles && git add . && git commit -m "nix change" && git push)
     (cd ~/nixconfig && nix flake lock --update-input dotfiles)
     nixos-rebuild switch --flake $HOME/nixconfig --use-remote-sudo
 }
