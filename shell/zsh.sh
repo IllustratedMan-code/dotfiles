cd305766520814a87a5bf7ca831a63fdb4ca99b4
lew2mz <david.lewis@cchmc.org>
2022-09-07 09:56:37 -0400

    nix change

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -1,7 +1,10 @@
-#PS1='%F{green}%~%f > '
+# this will get overwritten if oh-my-zsh is used
+PS1='%F{green}%~%f > '
 eval "$(direnv hook zsh)"
 
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 setopt INC_APPEND_HISTORY
+
+DEFAULT_USER=$USER
