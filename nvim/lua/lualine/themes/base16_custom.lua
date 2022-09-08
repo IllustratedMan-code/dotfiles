c5c20856153dbd0d2c5ed5d9ccc014e4ddeb8313
lew2mz <david.lewis@cchmc.org>
2022-09-08 11:19:10 -0400

    nix change

--- nvim/lua/lualine/themes/base16_custom.lua
+++ nvim/lua/lualine/themes/base16_custom.lua
@@ -22,40 +22,40 @@ end
 
 
 
-local base16 = {}
+local base16_custom = {}
 
-base16.normal = {
+base16_custom.normal = {
 	a = { fg = colors.base01, bg = colors.base0D },
 	b = { fg = colors.base05, bg = colors.base02 },
 	c = { fg = colors.base04, bg = colors.base01 },
 }
 
-base16.insert = {
+base16_custom.insert = {
 	a = { fg = colors.base01, bg = colors.base04 },
 	b = { fg = colors.base06, bg = colors.base02 },
 	y = {  fg = colors.base05, bg = colors.base02 },
 }
 
-base16.visual = {
+base16_custom.visual = {
 	a = { fg = colors.base00, bg = colors.base07 },
 	b = { fg = colors.base04, bg = colors.base02 },
 	y = {  fg = colors.base05, bg = colors.base02 },
 }
 
-base16.replace = {
+base16_custom.replace = {
 	a = { fg = colors.base00, bg = colors.base09 },
 	b = { fg = colors.base04, bg = colors.base02 },
 	y = {  fg = colors.base05, bg = colors.base02 },
 }
 
 
-base16.inactive = {
+base16_custom.inactive = {
 	a = { fg = colors.base04, bg = colors.base00, gui = "bold" },
 	b = { fg = colors.base04, bg = colors.base00 },
 	c = { fg = colors.base04, bg = colors.base01 },
 }
 
-base16.command = base16.insert
-base16.terminal = base16.insert
+base16_custom.command = base16_custom.insert
+base16_custom.terminal = base16_custom.insert
 
-return base16
+return base16_custom
