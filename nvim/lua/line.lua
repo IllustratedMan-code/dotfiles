1c5def5317475f24895f10ab59b66b18b637bfe6
lew2mz <david.lewis@cchmc.org>
2022-09-08 11:17:10 -0400

    nix change

--- nvim/lua/line.lua
+++ nvim/lua/line.lua
@@ -1,7 +1,7 @@
 require'lualine'.setup {
   options = {
     icons_enabled = true,
-    theme = 'base16',
+    theme = 'base16_custom',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {},
