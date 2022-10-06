867eb3199a6bdf5b2daac38211e5e6dfae0e07e2
lew2mz <david.lewis@cchmc.org>
2022-10-06 14:17:19 -0400

    nix change

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,18 +1,19 @@
 local lsp = require "lspconfig"
 local coq = require "coq"
 
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-lsp.hls.setup{coq.lsp_ensure_capabilities()}
-lsp.pyright.setup{coq.lsp_ensure_capabilities()}
-lsp.bashls.setup{coq.lsp_ensure_capabilities()}
-lsp.ccls.setup{coq.lsp_ensure_capabilities()}
-lsp.rust_analyzer.setup{coq.lsp_ensure_capabilities()}
-lsp.texlab.setup{coq.lsp_ensure_capabilities()}
-lsp.r_language_server.setup{coq.lsp_ensure_capabilities()}
-lsp.html.setup{coq.lsp_ensure_capabilities()}
-lsp.eslint.setup{coq.lsp_ensure_capabilities()}
-lsp.gdscript.setup{coq.lsp_ensure_capabilities()}
-lsp.r_language_server.setup{coq.lsp_ensure_capabilities()}
-lsp.emmet_ls.setup{coq.lsp_ensure_capabilities()}
-lsp.rnix.setup{coq.lsp_ensure_capabilities()}
-lsp.sumneko_lua.setup{coq.lsp_ensure_capabilities()}
+lsp.hls.setup{capabilities=capabilities}
+lsp.pyright.setup{capabilities=capabilities}
+lsp.bashls.setup{capabilities=capabilities}
+lsp.ccls.setup{capabilities=capabilities}
+lsp.rust_analyzer.setup{capabilities=capabilities}
+lsp.texlab.setup{capabilities=capabilities}
+lsp.r_language_server.setup{capabilities=capabilities}
+lsp.html.setup{capabilities=capabilities}
+lsp.eslint.setup{capabilities=capabilities}
+lsp.gdscript.setup{capabilities=capabilities}
+lsp.r_language_server.setup{capabilities=capabilities}
+lsp.emmet_ls.setup{capabilities=capabilities}
+lsp.rnix.setup{capabilities=capabilities}
+lsp.sumneko_lua.setup{capabilities=capabilities}
