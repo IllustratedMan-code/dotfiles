4b1b10061340e8ce09fb81f25e04280f0d2476b1
lew2mz <david.lewis@cchmc.org>
2022-10-12 15:55:29 -0400

    nix change

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,10 +4,10 @@ require('nvim-treesitter.configs').setup {
     },
     indent = {
         enable = true
-    }
+    },
     rainbow = {
       enable = true,
       extended_mode = true,
-      max_file_lines = nil,
+      max_file_lines = nil
     }
 }
