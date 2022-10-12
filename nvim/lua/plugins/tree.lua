0bfd2f907771e0c85fd356dace7ec38825818bd2
lew2mz <david.lewis@cchmc.org>
2022-10-12 13:58:08 -0400

    nix change

--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -1,4 +1,42 @@
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+local keymap = Hydra({
+   name = 'Telescope',
+   config = {
+      color = 'teal',
+      invoke_on_body = true,
+      hint = {
+         position = 'middle',
+         border = 'rounded',
+      },
+   },
+   mode = 'n',
+   body = '<Leader>f',
+   heads = {
+      { 'bmv', cmd 'bulk move' },
+      { 'C-]', cmd 'change directory' },
+      { 'o', cmd 'Telescope oldfiles', { desc = 'recently opened files' } },
+      { 'h', cmd 'Telescope help_tags', { desc = 'vim help' } },
+      { 'm', cmd 'MarksListBuf', { desc = 'marks' } },
+      { 'k', cmd 'Telescope keymaps' },
+      { 'O', cmd 'Telescope vim_options' },
+      { 'r', cmd 'Telescope resume' },
+      { 'p', cmd 'Telescope projects', { desc = 'projects' } },
+      { '/', cmd 'Telescope current_buffer_fuzzy_find', { desc = 'search in file' } },
+      { '?', cmd 'Telescope search_history',  { desc = 'search history' } },
+      { ';', cmd 'Telescope command_history', { desc = 'command-line history' } },
+      { 'c', cmd 'Telescope commands', { desc = 'execute command' } },
+      { 'u', cmd 'silent! %foldopen! | UndotreeToggle', { desc = 'undotree' }},
+      { '<Enter>', cmd 'Telescope', { exit = true, desc = 'list all pickers' } },
+      { '<Esc>', nil, { exit = true, nowait = true } },
+   }
+})
+
+require("nvim-tree").setup(
+    view = {
+      mappings = {
+        { key = "?", action = keymap:activate() }
+      },
+    },
+)
