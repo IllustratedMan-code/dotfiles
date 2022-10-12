60e5126984df328c5b26291d440f138b13cee73f
lew2mz <david.lewis@cchmc.org>
2022-10-12 16:38:44 -0400

    nix change

--- nvim/lua/config.vim
+++ nvim/lua/config.vim
@@ -1,1 +1,2 @@
 set nowrap
+set tabstop=2
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,29 +2,30 @@ local lsp = require "lspconfig"
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-lsp.hls.setup{capabilities=capabilities}
-lsp.pyright.setup{capabilities=capabilities}
-lsp.bashls.setup{capabilities=capabilities}
-lsp.ccls.setup{capabilities=capabilities}
-lsp.rust_analyzer.setup{capabilities=capabilities}
-lsp.texlab.setup{capabilities=capabilities}
-lsp.r_language_server.setup{capabilities=capabilities}
-lsp.html.setup{capabilities=capabilities}
-lsp.eslint.setup{capabilities=capabilities}
-lsp.gdscript.setup{capabilities=capabilities}
-lsp.emmet_ls.setup{capabilities=capabilities}
-lsp.rnix.setup{capabilities=capabilities}
-lsp.sumneko_lua.setup{
-  capabilities=capabilities,
-  Lua = {
-  settings = {
-    diagnostics = {
-      globals = { 'vim' },
-    },
-  },
-  },
+
+lsp.hls.setup { capabilities = capabilities }
+lsp.pyright.setup { capabilities = capabilities }
+lsp.bashls.setup { capabilities = capabilities }
+lsp.ccls.setup { capabilities = capabilities }
+lsp.rust_analyzer.setup { capabilities = capabilities }
+lsp.texlab.setup { capabilities = capabilities }
+lsp.r_language_server.setup { capabilities = capabilities }
+lsp.html.setup { capabilities = capabilities }
+lsp.eslint.setup { capabilities = capabilities }
+lsp.gdscript.setup { capabilities = capabilities }
+lsp.emmet_ls.setup { capabilities = capabilities }
+lsp.rnix.setup { capabilities = capabilities }
+lsp.sumneko_lua.setup {
+	capabilities = capabilities,
+	Lua = {
+		settings = {
+			diagnostics = {
+				globals = { 'vim' },
+			},
+		},
+	},
 }
-lsp.ltex.setup{capabilities=capabilities}
+lsp.ltex.setup { capabilities = capabilities }
 
 -- format buffer on save
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
