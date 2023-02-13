90eeb40debeae28681f586c1660f88bd6af978da
David Lewis <davidalewis00@gmail.com>
2023-02-13 08:12:37 -0500

    nix change

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -90,7 +90,7 @@
        direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
-       ein               ; tame Jupyter notebooks with emacs
+       ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        (lookup +dictionary +offline)              ; navigate your code and its documentation
